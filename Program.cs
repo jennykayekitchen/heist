95a9552080f5299e57e7cdc2624ee54e6fb946f7
Jenny Kitchen <jennykayekitchen@gmail.com>
2023-03-02 15:43:46 -0600

    phase 1 complete

--- Program.cs
+++ Program.cs
@@ -8,9 +8,15 @@ namespace Heist
         {
             Console.WriteLine("Plan Your Heist!");
             Console.WriteLine("What is your name?");
-            TeamMember theTeamMember = new TeamMember(Console.ReadLine(), teamMemberSkill, teamMemberCourage);
-
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your skill level?");
+            int skill = int.Parse(Console.ReadLine());
+            Console.WriteLine("On a scale of 0.0 - 2.0, what is your courage factor?");
+            double courage = Convert.ToDouble(Console.ReadLine());
             
+            TeamMember newMember = new TeamMember(name, skill, courage);
+
+            Console.WriteLine($"{newMember.Name} has a skill level of {newMember.SkillLevel} and a courage factor of {newMember.CourageFactor}.");
         }
     }
 }
